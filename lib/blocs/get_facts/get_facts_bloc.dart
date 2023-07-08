cb81b0fdb25e75844f0d5f973cbde0521b4cafdf
Abdulazizbek2 <abdulazizbek2000boy@gmail.com>
2023-07-09 00:24:18 +0500

    bloc rew

--- lib/blocs/get_facts/get_facts_bloc.dart
+++ lib/blocs/get_facts/get_facts_bloc.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:cats_fact/core/dio/dio_error_exception.dart';
 import 'package:cats_fact/src/repo/repository.dart';
 import 'package:dio/dio.dart';
@@ -7,7 +5,6 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../models/cat_model.dart/cat_model.dart';
-
 
 part 'get_facts_event.dart';
 part 'get_facts_state.dart';
@@ -20,14 +17,14 @@ class GetFactsBloc extends Bloc<GetFactsEvent, GetFactsState> {
     on<_GetFacts>(_getFacts);
   }
 
-
   Future<void> _getFacts(_GetFacts event, Emitter<GetFactsState> emit) async {
     emit(GetFactsLoading());
-    try{
+    try {
       final res = await catFactRepo.getFacts();
       emit(GetFactsSuccess(catsModelList: res));
-    } on DioException catch(e){
-      emit(GetFactsError(errorMessage: DioExceptions.fromDioError(e).toString()));
+    } on DioException catch (e) {
+      emit(GetFactsError(
+          errorMessage: DioExceptions.fromDioError(e).toString()));
     }
   }
 }
